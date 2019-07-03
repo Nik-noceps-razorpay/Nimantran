5f7c1da492e6692791455eb53457e1ea46220038
Nik_Noceps <nikitesh.soneji@razorpay.com>
2019-07-03 19:21:36 +0530

    created models for users and events and a pivot table for invites

---
+++ app/Event.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    //
+    protected $fillable = [
+        'name', 'details', 'location' ,'dateTime'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+}
---
+++ database/migrations/2019_07_03_134122_create_events_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events_users', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->integer('users_id');
+            $table->integer('events_id');
+            $table->integer('invite_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events_users', function (Blueprint $table) {
+            //
+        });
+    }
+}
